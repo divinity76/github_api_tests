ad6554ec8283dc97f02d7b30c1a115db0a47dedf
hans <divinity76@gmail.com>
2020-02-02 16:07:39 +0100

    test

--- src/webroot/github_callback_endpoint.php
+++ src/webroot/github_callback_endpoint.php
@@ -2,10 +2,11 @@
 declare(strict_types = 1);
 
 // generating a secure token:
-// php -r '$token=base64_encode(random_bytes(20));echo "token: ".$token.PHP_EOL."php-ified pre-computed sha1-hash: \"\\x".implode("\\x",str_split(bin2hex(hash("sha1",$token,true)),2))."\"".PHP_EOL;'
-// token: TmzmMMQJPIyguIR4vypjfflBe8o=
-// php-ified pre-computed sha1-hash: "\x31\x34\xf6\x29\xdc\x41\xfb\x00\x7c\x79\xc7\xf2\x37\xb9\x8a\x79\x37\x98\x21\x33"
-const TOKEN_SHA1 = "\x31\x34\xf6\x29\xdc\x41\xfb\x00\x7c\x79\xc7\xf2\x37\xb9\x8a\x79\x37\x98\x21\x33";
+// php -r '$token=base64_encode(random_bytes(20));echo "token: ".$token.PHP_EOL."token urlencoded: ".urlencode($token).PHP_EOL."php-ified pre-computed sha1-hash: \"\\x".implode("\\x",str_split(bin2hex(hash("sha1",$token,true)),2))."\"".PHP_EOL;'
+// token: 2803vTRrMSTIJya5eEynKoNg/WU=
+// token urlencoded: 2803vTRrMSTIJya5eEynKoNg%2FWU%3D
+// php-ified pre-computed sha1-hash: "\xe8\xe0\x02\x18\x92\xb4\xbf\x00\xed\x79\x19\x02\x64\xd2\x65\x4a\x23\x37\x58\xfc"
+const TOKEN_SHA1 = "\xe8\xe0\x02\x18\x92\xb4\xbf\x00\xed\x79\x19\x02\x64\xd2\x65\x4a\x23\x37\x58\xfc";
 
 header("Content-Type: text/plain;charset=UTF-8");
 
